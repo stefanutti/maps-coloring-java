4d2d4a7ef050655bba6e6d6b90fb6fa17590a488
Mario Stefanutti <mario.stefanutti@gmail.com>
2011-03-09 02:56:59 +0100

    Small corrections

---
+++ ct/ct-ui-swixml/src/main/java/it/tac/ct/ui/swixml/tests/Flickering.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright 2011 by Mario Stefanutti, released under GPLv3.
+ */
+package it.tac.ct.ui.swixml.tests;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.net.URL;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.RepaintManager;
+
+import no.geosoft.cc.geometry.Geometry;
+import no.geosoft.cc.graphics.GObject;
+import no.geosoft.cc.graphics.GPosition;
+import no.geosoft.cc.graphics.GSegment;
+import no.geosoft.cc.graphics.GStyle;
+import no.geosoft.cc.graphics.GText;
+import no.geosoft.cc.graphics.GWindow;
+
+import org.swixml.SwingEngine;
+
+public class Flickering extends JFrame {
+
+    private final JPanel mapExplorer = null;
+
+    public Flickering() {
+        try {
+            SwingEngine<Flickering> engine = new SwingEngine<Flickering>(this);
+            URL configFileURL = this.getClass().getClassLoader().getResource("config/4ct-test-flickering.xml");
+            engine.render(configFileURL);
+
+            // Create the graphic canvas
+            GWindow window = new GWindow();
+
+            // HERE IS THE WORKAROUND
+            //
+            RepaintManager.currentManager(this).setDoubleBufferingEnabled(true);
+            // Create scene with default viewport and world extent settings
+            // GScene scene = new GScene(window);
+
+            // mapExplorer.add(window.getCanvas());
+
+            // Create the graphics object and add to the scene
+            // GObject helloWorld = new HelloWorld();
+            // scene.add(helloWorld);
+
+            // Get visible
+            //
+            validate();
+            setVisible(true);
+
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    /**
+     * Defines the geometry and presentation for the sample graphic object.
+     */
+    public class HelloWorld extends GObject {
+        private GSegment star_;
+
+        public HelloWorld() {
+            star_ = new GSegment();
+            GStyle starStyle = new GStyle();
+            starStyle.setForegroundColor(new Color(255, 0, 0));
+            starStyle.setBackgroundColor(new Color(255, 255, 0));
+            starStyle.setLineWidth(3);
+            setStyle(starStyle);
+            addSegment(star_);
+
+            GText text = new GText("HelloWorld", GPosition.MIDDLE);
+            GStyle textStyle = new GStyle();
+            textStyle.setForegroundColor(new Color(100, 100, 150));
+            textStyle.setBackgroundColor(null);
+            textStyle.setFont(new Font("Dialog", Font.BOLD, 48));
+            text.setStyle(textStyle);
+            star_.setText(text);
+        }
+
+        /**
+         * This method is called whenever the canvas needs to redraw this object. For efficiency, prepare as much of the graphic object up front (such as sub object, segment and style setup) and set geometry only in this method.
+         */
+        public void draw() {
+            star_.setGeometry(Geometry.createStar(220, 220, 200, 80, 15));
+        }
+    }
+
+    public static void main(String[] args) {
+        new Flickering();
+    }
+}
---
+++ ct/ct-ui-swixml/src/main/java/it/tac/ct/ui/swixml/tests/SwixmlUITester.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2011 by Mario Stefanutti, released under GPLv3.
+ */
+package it.tac.ct.ui.swixml.tests;
+
+import java.net.URL;
+
+import javax.swing.JFrame;
+
+import org.swixml.SwingEngine;
+
+public class SwixmlUITester extends JFrame {
+
+	public SwixmlUITester() {
+		try {
+			SwingEngine<SwixmlUITester> engine = new SwingEngine<SwixmlUITester>(this);
+			URL configFileURL = this.getClass().getClassLoader().getResource("config/4ct-test-snap.xml");
+			engine.render(configFileURL).setVisible(true);
+		} catch (Exception exception) {
+			exception.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		new SwixmlUITester();
+	}
+}
